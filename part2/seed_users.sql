caa11bc02c42e219380c31b1d8c888ff0e5fa003
qtr2612 <tranquan26122005@gmail.com>
2025-06-21 06:18:28 +0000

    [automatic commit] 2025-06-21 06:18:28 UTC 116.204.144.67

---
+++ part2/seed_users.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Users (username, email, password_hash, role, created_at)
+VALUES
+  ('ownerJane', 'jane@example.com', 'hashedpassword123', 'owner', '2025-06-06 01:32:58'),
+  ('walkerMike', 'mike@example.com', 'hashedpassword456', 'walker', '2025-06-06 01:32:58'),
+  ('ownerBob', 'bob@example.com', 'hashedpassword789', 'owner', '2025-06-06 01:34:32');
